6d9e454a1bc7d43197c50a3e3440797c9dc7c803
Kartik Desai <desaikartik99@gmail.com>
2020-03-08 06:30:02 +0530

    Forth

--- app/src/main/java/com/example/eroe/Landing.java
+++ app/src/main/java/com/example/eroe/Landing.java
@@ -1,5 +1,4 @@
 package com.example.eroe;
-
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
@@ -10,13 +9,25 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
 import android.util.Log;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
-;import static android.content.Context.LOCATION_SERVICE;
+;import java.io.IOException;
+import java.util.Random;
+
+import static android.content.Context.LOCATION_SERVICE;
 
 public class Landing extends AppCompatActivity {
     private String SOS_MESSAGE=" IS IN AN EMERGENCY AND NEEDS YOUR HELP. PLEASE HELP THEM." +
@@ -26,7 +37,14 @@ public class Landing extends AppCompatActivity {
     private LocationListener locationListener;
     private final long MIN_TIME = 1000;
     private final long MIN_DIST = 5;
-
+    Button buttonStart, buttonStop, buttonPlayLastRecordAudio,
+            buttonStopPlayingRecording ;
+    String AudioSavePathInDevice = null;
+    MediaRecorder mediaRecorder ;
+    Random random ;
+    String RandomAudioFileName = "ABCDEFGHIJKLMNOP";
+    public static final int RequestPermissionCode = 1;
+    MediaPlayer mediaPlayer ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +54,14 @@ public class Landing extends AppCompatActivity {
 
 
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
+        buttonStart = (Button) findViewById(R.id.button);
+        buttonStop = (Button) findViewById(R.id.button2);
+        buttonPlayLastRecordAudio = (Button) findViewById(R.id.button3);
+        buttonStopPlayingRecording = (Button)findViewById(R.id.button4);
+
+        buttonStop.setEnabled(false);
+        buttonPlayLastRecordAudio.setEnabled(false);
+        buttonStopPlayingRecording.setEnabled(false);
 
         locationListener = new LocationListener() {
             @RequiresApi(api = Build.VERSION_CODES.DONUT)
@@ -43,7 +69,9 @@ public class Landing extends AppCompatActivity {
             public void onLocationChanged(Location location) {
                 Log.d("long", String.valueOf(location.getLongitude()));
                 ActivityCompat.requestPermissions(Landing.this,new String[]{Manifest.permission.SEND_SMS},1);
-                SmsManager.getDefault().sendTextMessage("09869207983",null, String.valueOf(location.getLongitude()),null,null);
+
+                Toast.makeText(Landing.this, "Longitutde: "+String.valueOf(location.getLongitude()+"\n Latitude: "+String.valueOf(location.getLatitude())),
+                        Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -61,6 +89,7 @@ public class Landing extends AppCompatActivity {
 
             }
         };
+
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         try {
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME,MIN_DIST,locationListener);
@@ -69,6 +98,154 @@ public class Landing extends AppCompatActivity {
         catch (SecurityException e){
             e.printStackTrace();
         }
+
+        random = new Random();
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(checkPermission()) {
+
+                    AudioSavePathInDevice =
+                            Environment.getExternalStorageDirectory().getAbsolutePath() + "/" +
+                                    CreateRandomAudioFileName(5) + "AudioRecording.3gp";
+
+                    MediaRecorderReady();
+
+                    try {
+                        mediaRecorder.prepare();
+                        mediaRecorder.start();
+                    } catch (IllegalStateException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    buttonStart.setEnabled(false);
+                    buttonStop.setEnabled(true);
+
+                    Toast.makeText(Landing.this, "Recording started",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    requestPermission();
+                }
+
+            }
+        });
+
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaRecorder.stop();
+                buttonStop.setEnabled(false);
+                buttonPlayLastRecordAudio.setEnabled(true);
+                buttonStart.setEnabled(true);
+                buttonStopPlayingRecording.setEnabled(false);
+
+
+            }
+        });
+
+        buttonPlayLastRecordAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) throws IllegalArgumentException,
+                    SecurityException, IllegalStateException {
+
+                buttonStop.setEnabled(false);
+                buttonStart.setEnabled(false);
+                buttonStopPlayingRecording.setEnabled(true);
+
+                mediaPlayer = new MediaPlayer();
+                try {
+                    mediaPlayer.setDataSource(AudioSavePathInDevice);
+                    mediaPlayer.prepare();
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                mediaPlayer.start();
+                Toast.makeText(Landing.this, "Recording Playing",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        buttonStopPlayingRecording.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonStop.setEnabled(false);
+                buttonStart.setEnabled(true);
+                buttonStopPlayingRecording.setEnabled(false);
+                buttonPlayLastRecordAudio.setEnabled(true);
+
+                if(mediaPlayer != null){
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                    MediaRecorderReady();
+                }
+            }
+        });
+
+    }
+
+    public void MediaRecorderReady(){
+        mediaRecorder=new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        mediaRecorder.setOutputFile(AudioSavePathInDevice);
+    }
+
+    public String CreateRandomAudioFileName(int string){
+        StringBuilder stringBuilder = new StringBuilder( string );
+        int i = 0 ;
+        while(i < string ) {
+            stringBuilder.append(RandomAudioFileName.
+                    charAt(random.nextInt(RandomAudioFileName.length())));
+
+            i++ ;
+        }
+        return stringBuilder.toString();
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(Landing.this, new
+                String[]{WRITE_EXTERNAL_STORAGE, RECORD_AUDIO}, RequestPermissionCode);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case RequestPermissionCode:
+                if (grantResults.length> 0) {
+                    boolean StoragePermission = grantResults[0] ==
+                            PackageManager.PERMISSION_GRANTED;
+                    boolean RecordPermission = grantResults[1] ==
+                            PackageManager.PERMISSION_GRANTED;
+
+                    if (StoragePermission && RecordPermission) {
+                        Toast.makeText(Landing.this, "Permission Granted",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(Landing.this,"Permission Denied",Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+        }
+    }
+
+    public boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(),
+                WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(),
+                RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED &&
+                result1 == PackageManager.PERMISSION_GRANTED;
+    }
+}
+    /*
         String[] contactList=null;
         String location="";
         SQLiteDatabase db =  openOrCreateDatabase("Eroe.db", Context.MODE_PRIVATE, null);
@@ -87,7 +264,7 @@ public class Landing extends AppCompatActivity {
             Log.d("dest",contactList[3]);
             Log.d("dest",contactList[4]);
             */
-            for(int i=0;i<contactList.length;i++){
+       /*     for(int i=0;i<contactList.length;i++){
                 if(location==null)
                     sendMessage(contactList[i],"Location unavailable");
                 else
@@ -102,6 +279,6 @@ public class Landing extends AppCompatActivity {
         String messageToSend= SOS_MESSAGE+location;
         Log.d("dest","0"+number);
         //SmsManager.getDefault().sendTextMessage("0"+number, null, messageToSend, null,null);
-    }
+    }*/
 
-}
+
